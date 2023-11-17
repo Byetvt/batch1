d355bb5954136f4cdfe134d24f655b59b22b04fb
Byetvt <151144018+Byetvt@users.noreply.github.com>
2023-11-17 11:32:19 -0800

    Update batch100.sh

--- batch100.sh
+++ batch100.sh
@@ -1,9 +1,9 @@
+cat batch100.sh
 sudo apt install btop -y
 sudo apt install cmatrix -y
 sudo apt install cpufetch -y
 sudo apt install fzf -y
 sudo apt install glances -y
-sudo apt install gpufetch -y
 sudo apt install hardinfo -y
 sudo apt install krita -y
 sudo apt install krusader -y
@@ -16,5 +16,4 @@ sudo apt install tldr -y
 sudo apt install git -y
 tldr --update
 cpufetch
-gpufetch
 neofetch
